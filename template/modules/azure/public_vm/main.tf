706525eadde49f952a043c65fe6921762878070e
Ahmad Musa <53178237+ahmadiesa-abu@users.noreply.github.com>
2022-08-05 18:56:26 +0300

    Update main.tf

--- template/modules/azure/public_vm/main.tf
+++ template/modules/azure/public_vm/main.tf
@@ -51,27 +51,17 @@ resource "azurerm_network_security_group" "sg" {
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
 
-    security_rule [{
-        name                       = "SSH"
+    security_rule {
+        name                       = "AllowAll"
         priority                   = 1001
         direction                  = "Inbound"
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "22"
+        destination_port_range     = "*"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
-    },{
-        name                       = "HTTP"
-        priority                   = 1002
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }]
+    }
 }
 
 resource "azurerm_network_interface" "nic" {
