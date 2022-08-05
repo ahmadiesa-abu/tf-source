4e0cf0ceeb8b956b63115b890a71527079bf39af
Ahmad Musa <53178237+ahmadiesa-abu@users.noreply.github.com>
2022-08-05 18:03:32 +0300

    Update main.tf

--- template/modules/azure/public_vm/main.tf
+++ template/modules/azure/public_vm/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_public_ip" "fip" {
     name                = "${var.prefix}-ip-${random_id.suffix.hex}"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
-    depends_on = [azurerm_resource_group.rg]
+    depends_on = [azurerm_public_ip.fip, azurerm_linux_virtual_machine.vm]
     allocation_method   = "Dynamic"
 }
 
